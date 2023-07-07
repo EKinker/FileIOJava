7ce95498955ec12a1f048412149f8617674a7be3
Liz Kinker <Liz.Kinker@gmail.com>
2023-07-07 18:40:35 -0400

    Added pom.xml and did step1

--- Step1/ReadOne.java
+++ Step1/ReadOne.java
@@ -6,13 +6,14 @@ public class ReadOne
 {
     public static void main(String[] args)
     {
+
         /* We must use a try/catch when reading 
          * in a file in case the file is not found
          */
         try {
             // Create the Scanner object using the file
             // as input
-            Scanner fileIn = new Scanner(new File("input.txt"));
+            Scanner fileIn = new Scanner(new File("/Users/liz/LoPro/FileIOJava/Step1/input.txt"));
             
             /* Loop while the file still has lines.
              * .hasNext() looks to see if a line exists
